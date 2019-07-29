0cdb2e3551c133784f184fe84a56596767b4fd51
myname21-meet <myname21@meet.mit.edu>
2019-07-29 07:15:10 -0400

    we

--- knowledge_databases.py
+++ knowledge_databases.py
@@ -8,14 +8,31 @@ Base.metadata.create_all(engine)
 DBSession = sessionmaker(bind=engine)
 session = DBSession()
 
-def add_article():
-	pass
+def add_article(name,topic,rating):
+	artical_object = Knowledge(
+		name = name,
+		topic=topic,
+		rating=rating)
+	session.add(artical_object)
+	session.commit()
+
+
+add_article("health" , "psychology" , 9)
+add_article("body" , "biology" , 10)
 
 def query_all_articles():
-	pass
+	articals = session.query(
+		Knowledge).all()
+	return articals
+
+print(query_all_articles())
+	
 
 def query_article_by_topic():
-	pass
+	articals = session.query(
+		Knowledge).filter_by(
+		topic=their_topic)
+	return articals
 
 def delete_article_by_topic():
 	pass
--- knowledge_model.py
+++ knowledge_model.py
@@ -6,12 +6,24 @@ from sqlalchemy import create_engine
 Base = declarative_base()
 
 class Knowledge(Base):
-	# Create a table with 4 columns
-	# The first column will be the primary key
-	# The second column should be a string representing
-	# the name of the Wiki article that you're referencing
-	# The third column will be a string representing the 
-	# topic of the article. The last column will be
-	# an integer, representing your rating of the article.
+	__tablename__ = "topics"
+	article_num =Column(Integer,primary_key=True)
+	name=Column(String)
+	topic=Column(String)
+	rating=Column(Integer)
 
-	pass
+
+	def __repr__(self):
+		return("article name: {}\n"
+				"article topic:{}\n"
+				"article rating{}\n").format(
+					self.name,
+					self.topic,
+					self.rating)
+		print("If you want to learn about" , self.topic , "you should look at the Wikipedia article called" , self.name
+		"we gave this artical a rating of " , self.rating ,"out of 10")
+
+
+x = Knowledge(name="what happens when you're angry",topic="psychology",rating=9)
+y = Knowledge(name= "rainbow",topic= "weather",rating=9)
+
